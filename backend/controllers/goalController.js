c894d27253c73edaa90ba77a4b5e8db6214baac4
Dipankar Saikia <er.dipankarsaikia@gmail.com>
2022-03-20 10:32:31 +0530

    error middleware, async handler async await are added

---
+++ backend/controllers/goalController.js
@@ -0,0 +1,43 @@
+const asyncHandler = require('express-async-handler')
+
+// @desc   Get Goals
+// @route  GET /api/goals
+// @access Private
+const getGoals = asyncHandler(async (req, res) => {
+    res.status(200).json({message: 'Get Goals'})
+})
+
+// @desc   Set Goal
+// @route  POST /api/goals
+// @access Private
+const setGoal = asyncHandler(async (req, res) => {
+    // console.log(req.body)
+    if(!req.body.text){
+        // res.status(400).json({message: 'please add a text field'})
+        res.status(400)
+        throw new Error('please send a text field')
+    }
+
+    res.status(200).json({message: 'Create new Goal'})
+})
+
+// @desc   Update Goal
+// @route  PUT /api/goals/:id
+// @access Private
+const updateGoal = asyncHandler(async (req, res) => {
+    res.status(200).json({message: `Update goal ${req.params.id}`})
+})
+
+// @desc   Delete Goal
+// @route  GET /api/goal/:id
+// @access Private
+const deleteGoal = asyncHandler(async (req, res) => {
+    res.status(200).json({message: `Delete goal ${req.params.id}`})
+})
+
+module.exports = {
+    getGoals,
+    setGoal,
+    updateGoal,
+    deleteGoal
+}
--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,20 @@
-console.log('hello world')
+// console.log('hello world')
+const express = require('express')  // express is the backend web framework
+const dotenv = require('dotenv').config()  // allows us to have a .env file at the root with our environment variables
+const {errorHandler} = require('./middleware/errorMiddleware')
+const port = process.env.port || 5000
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+// app.get('/api/goals', (req, res) => {
+//     // res.send('Get Goals')
+//     res.json({message: 'Get Goals'})
+// })
+app.use('/api/goals', require('./routes/goalRoutes'))
+
+app.use(errorHandler)
+
+app.listen(port, () => console.log(`server started in port ${port}`))
