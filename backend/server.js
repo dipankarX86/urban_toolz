14a6453965a516b51a9c9e90c6f6b3905f3be535
Dipankar Saikia <er.dipankarsaikia@gmail.com>
2022-04-29 20:54:08 +0530

    MPA finally worked

--- backend/server.js
+++ backend/server.js
@@ -21,11 +21,16 @@ app.use(express.urlencoded({extended: false}))
 app.use('/api/goals', require('./routes/goalRoutes'))
 app.use('/api/users', require('./routes/userRoutes'))
 
+//app.use('/', express.static(path.join(__dirname, 'public')))    // this dont need to be static in express
+                                                                // just learn, how to access individual pages or pages 
+                                                                // through function in express router
+                                                                // read on express router more
+
 //  Serve frontend
 if(process.env.NODE_ENVIRONMENT === 'production') {
-    app.use(express.static(path.join(__dirname, '../frontend/build')))
+    app.use('/', express.static(path.join(__dirname, '../frontend/build')))
 
-    app.get('*', (req, res) => 
+    app.get('/dashboard', (req, res) => 
         res.sendFile(
             path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
         )
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -26,7 +26,7 @@ function Login() {
     }
 
     if(isSuccess || user) {
-      navigate('/')
+      navigate('/dashboard')
     }
 
     dispatch(reset())
