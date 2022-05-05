ce013e22f31779ad36a65c03bfe12847f152c4b0
Dipankar Saikia <er.dipankarsaikia@gmail.com>
2022-05-05 23:08:10 +0530

    modal added for app install, how to pass data to and from in MPA model

---
+++ public/js/modal.js
@@ -0,0 +1,8 @@
+
+// BOOTSTRAP MODAL
+var myModal = document.getElementById('myModal')
+var myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', function () {
+    myInput.focus()
+})
